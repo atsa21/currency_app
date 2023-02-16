6e221ebf54e05ebdba3a7ba4603a1d2a66fb88d7
atsa21 <ts21ann@gmail.com>
2023-02-17 01:49:17 +0200

    fixed change currency to convert right

--- src/app/components/main-page/converter-page/converter-page.component.ts
+++ src/app/components/main-page/converter-page/converter-page.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Currency } from 'src/app/models/currency';
 import { CurrencyService } from 'src/app/services/currency.service';
-import { Patterns } from 'src/assets/patterns/patterns';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
@@ -31,8 +30,8 @@ export class ConverterPageComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.convertForm = this.fb.group({
-      firstAmount: new FormControl('', [Validators.max(1000000000), Validators.pattern(Patterns.ValuePattern)]),
-      secondAmount: new FormControl('', [Validators.max(1000000000), Validators.pattern(Patterns.ValuePattern)]),
+      firstAmount: new FormControl('', [Validators.min(0.01)]),
+      secondAmount: new FormControl('', [Validators.min(0.01)]),
       firstCurrency: new FormControl('USD', Validators.required),
       secondCurrency: new FormControl('UAH', Validators.required)
     })
@@ -54,12 +53,16 @@ export class ConverterPageComponent implements OnInit, OnDestroy {
     return this.convertForm.get('secondCurrency');
   }
 
-  public convertCurrency(input: string, amount: number): void {
-    if(amount && this.convertForm.valid) {
-      const from = input === 'from' ? this.firstCurrency?.value : this.secondCurrency?.value;
-      const to = input === 'from' ? this.secondCurrency?.value : this.firstCurrency?.value;
-      this.currencyService.getConvCurrency(from, to, amount).pipe(takeUntil(this.destroy$)).subscribe(data => {
+  public convertCurrency(input: string, control: AbstractControl<any> | null): void {
+    if(control?.value && control.valid) {
+      let from = this.firstCurrency?.value;
+      let to = this.secondCurrency?.value;
+      if(input === 'to') {
+        [from, to] = [to, from];
+      }
+      this.currencyService.getConvCurrency(from, to, control.value).pipe(takeUntil(this.destroy$)).subscribe(data => {
         if(data) {
+          console.log(data);
           const res = JSON.stringify(data);
           const currjson = JSON.parse(res);
           input === 'from' ? this.secondAmount?.setValue(currjson.result) : this.firstAmount?.setValue(currjson.result);
--- src/assets/patterns/patterns.ts
+++
@@ -1,3 +0,0 @@
-export const Patterns = {
-    ValuePattern: /^(?!0\d*$)\d+(?:\.\d{1,2})?$/
-}
